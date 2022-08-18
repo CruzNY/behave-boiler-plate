279d8f120edd2e04e9170e55ecceff086407734e
Alexis Cruz <alexcruz182@gmail.com>
2022-08-17 22:27:17 -0400

    refector to context brwoser

--- features/environment.py
+++ features/environment.py
@@ -16,12 +16,13 @@ def before_all(context):
     config.read(my_file)
  
     # Reading the browser type from the configuration file for Selenium Python Tutorial
-    helper_func = get_browser(config.get('Environment', 'Browser'))
-    context.helperfunc = helper_func
+    browser_inst = get_browser(config.get('Environment', 'Browser'))
+    # TODO change this to browser
+    context.browser = browser_inst
     
     # Local Chrome WebDriver
     #if context.browser == "chrome":
     #   context.driver = webdriver.Chrome()
  
 def after_all(context):
-    context.helperfunc.close()
+    context.browser.close()
--- features/steps/TodoSteps.py
+++ features/steps/TodoSteps.py
@@ -8,25 +8,25 @@ from behave import given, when, then
 @given(u'I go to 4davanceboy to add item')
 def step(context):
     print('steps')
-    context.helperfunc.open('https://lambdatest.github.io/sample-todo-app/')
-    context.helperfunc.maximize()
+    context.browser.open('https://lambdatest.github.io/sample-todo-app/')
+    context.browser.maximize()
  
 @then(u'I Click on first checkbox and second checkbox')
 def click_on_checkbox_one(context):
-    context.helperfunc.find_by_name('li1').click()
-    context.helperfunc.find_by_name('li2').click()
+    context.browser.find_by_name('li1').click()
+    context.browser.find_by_name('li2').click()
  
 @when(u'I enter item to add')
 def enter_item_name(context):
-    context.helperfunc.find_by_id('sampletodotext').send_keys("Yey, Let's add it to list")
+    context.browser.find_by_id('sampletodotext').send_keys("Yey, Let's add it to list")
  
 @when(u'I click add button')
 def click_on_add_button(context):
-    context.helperfunc.find_by_id('addbutton').click()
+    context.browser.find_by_id('addbutton').click()
  
 @then(u'I should verify the added item')
 def see_login_message(context):
-    added_item = context.helperfunc.find_by_xpath("//span[@class='done-false']").text
+    added_item = context.browser.find_by_xpath("//span[@class='done-false']").text
  
     time.sleep(10)
  
